8a5e1c7cfb9861d1ccb612b887b5461d8b7df50e
ftnext <takuyafjp+develop@gmail.com>
2021-01-11 00:32:24 +0900

    [chore] add revealjs extension

--- source/conf.py
+++ source/conf.py
@@ -28,6 +28,7 @@ author = 'nikkie'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_revealjs',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
