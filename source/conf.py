5fa0b17ea6f9fa4974509e3635618a9f66348bdf
ftnext <takuyafjp+develop@gmail.com>
2021-01-13 00:00:33 +0900

    [chore] specify plugin and css which are needed in systax highlighting of Reveal.js (close #3)

--- source/conf.py
+++ source/conf.py
@@ -58,3 +58,16 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for Reveal.js output -------------------------------------------------
+
+revealjs_script_plugins = [
+    {
+        "name": "RevealHighlight",
+        "src": "revealjs4/plugin/highlight/highlight.js",
+    },
+]
+
+revealjs_css_files = [
+    "revealjs4/plugin/highlight/zenburn.css",
+]
