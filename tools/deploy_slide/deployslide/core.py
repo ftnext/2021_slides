7cf9a224fb043c4b01d75e44127bbf216f846535
ftnext <takuyafjp+develop@gmail.com>
2021-02-11 15:49:20 +0900

    [feat] Implement SlideDeployer factory

--- tools/deploy_slide/deployslide/core.py
+++ tools/deploy_slide/deployslide/core.py
@@ -1,4 +1,16 @@
+from dataclasses import dataclass
+
+from deployslide import naming_rules
+
+
+@dataclass
 class SlideDeployer:
+    html_rule: naming_rules.HtmlNamingRule
+    images_rule: naming_rules.ImagesNamingRule
+
     @classmethod
-    def create_from_entire_rules(cls, entire_rules):
-        raise NotImplementedError
+    def create_from_entire_rules(
+        cls, entire_rules: "naming_rules.EntireRules"
+    ):
+        """Factory from `naming_rules.EntireRules` instance."""
+        return cls(entire_rules.for_html, entire_rules.for_images)
