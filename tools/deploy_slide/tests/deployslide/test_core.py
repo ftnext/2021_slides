3f38c5e11c6071e32c7066dcd3df60f899105a94
ftnext <takuyafjp+develop@gmail.com>
2021-02-11 17:01:11 +0900

    [refactor] DRY SlideDeployer tests with setUp

--- tools/deploy_slide/tests/deployslide/test_core.py
+++ tools/deploy_slide/tests/deployslide/test_core.py
@@ -10,6 +10,15 @@ from deployslide.naming_rules import (
 
 
 class SlideDeployerTestCase(TestCase):
+    def setUp(self):
+        # Passing None causes TypeError in destination property.
+        # It seems that all properties are evaluated.
+        # ref: https://bugs.python.org/issue41768
+        self.html_rule = MagicMock(spec=HtmlNamingRule(""))
+        self.images_rule = MagicMock(spec=ImagesNamingRule())
+
+        self.deployer = sut.SlideDeployer(self.html_rule, self.images_rule)
+
     def test_create_from_entire_rules(self):
         entire_rules = MagicMock(spec=EntireRules(None, None))
 
@@ -24,35 +33,24 @@ class SlideDeployerTestCase(TestCase):
     @patch("deployslide.core.SlideDeployer._deploy_slide")
     @patch("deployslide.core.SlideDeployer._create_directories")
     def test_deploy(self, _create_directories, _deploy_slide, _copy_images):
-        # Passing None causes TypeError in destination property.
-        # It seems that all properties are evaluated.
-        # ref: https://bugs.python.org/issue41768
-        html_rule = MagicMock(spec=HtmlNamingRule(""))
-        images_rule = MagicMock(spec=ImagesNamingRule())
-
-        deployer = sut.SlideDeployer(html_rule, images_rule)
-        deployer.deploy()
+        self.deployer.deploy()
 
         _create_directories.assert_called_once_with()
         _deploy_slide.assert_called_once_with()
         _copy_images.assert_called_once_with()
 
     def test__create_directories(self):
-        html_rule = MagicMock(spec=HtmlNamingRule(""))
-        images_rule = MagicMock(spec=ImagesNamingRule())
+        self.deployer._create_directories()
 
-        deployer = sut.SlideDeployer(html_rule, images_rule)
-        deployer._create_directories()
-
-        html_rule.source.mkdir.assert_called_once_with(
+        self.html_rule.source.mkdir.assert_called_once_with(
             parents=True, exist_ok=True
         )
-        html_rule.destination.mkdir.assert_called_once_with(
+        self.html_rule.destination.mkdir.assert_called_once_with(
             parents=True, exist_ok=True
         )
-        images_rule.source.mkdir.assert_called_once_with(
+        self.images_rule.source.mkdir.assert_called_once_with(
             parents=True, exist_ok=True
         )
-        images_rule.destination.mkdir.assert_called_once_with(
+        self.images_rule.destination.mkdir.assert_called_once_with(
             parents=True, exist_ok=True
         )
