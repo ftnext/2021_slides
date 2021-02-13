7283aa879dac3cebcf10b836047b22fd5b1f213c
ftnext <takuyafjp+develop@gmail.com>
2021-02-14 01:11:47 +0900

    [chore] Bump up to 0.2.0 (CSS support)

--- tools/deploy_slide/setup.py
+++ tools/deploy_slide/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="deployslide",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(exclude=["tests.*", "tests"]),
     entry_points={"console_scripts": ["deployslide = deployslide:main"]},
 )
