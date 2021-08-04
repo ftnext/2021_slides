ef34ddea7aa0eacb9b107cde844012ace5a96926
ftnext <takuyafjp+develop@gmail.com>
2021-08-04 23:57:07 +0900

    [feat] Cached as wheel reimplementation

---
+++ samplecode/decorators/cached_decorator.py
@@ -0,0 +1,31 @@
+import functools
+import time
+
+
+def cache_enabled(func):
+    cache = {}
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        key = kwargs["number"]
+        if key in cache:
+            return cache[key]
+        retval = func(*args, **kwargs)
+        cache[key] = retval
+        return retval
+
+    return wrapper
+
+
+@cache_enabled
+def get_api(number):
+    time.sleep(2)
+    return number + 1
+
+
+if __name__ == "__main__":
+    for i in [1, 2, 1, 2, 3]:
+        # 2回めの1と2の呼び出しではキャッシュが使われるので2秒待たない
+        print("-" * 10)
+        print(get_api(number=i))
+        print("-" * 10)
