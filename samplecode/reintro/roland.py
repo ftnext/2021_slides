c0697d5c874ab8dc47d743e7861e6e948d665df2
ftnext <takuyafjp+develop@gmail.com>
2021-11-17 23:11:38 +0900

    [feat] If-statement trendy sample code

---
+++ samplecode/reintro/roland.py
@@ -0,0 +1,9 @@
+"""if文のサンプルプログラム
+
+ref: https://zenn.dev/kuramapommel/books/introduce_polymorphism/viewer/chapter2
+"""
+name = input("名前を入力してください: ")
+if name == "ローランド":
+    print("俺か")
+else:
+    print("俺以外か")
